511a1e0b7f53645fadf10dac27c54e8874ff162f
Dave Risney <dave@deletethis.net>
2014-05-01 09:12:11 -0700

    Removes unused remains of 'type' (fix #5) and only uses '(N)' suffix on visual nodes when necessary (fix #11)

--- js/codeStore.js
+++ js/codeStore.js
@@ -33,42 +33,22 @@
             },
             processDataAsync = function (data) {
                 var dataProperties = toProperties(data),
-                    allTypes = [],//dataProperties.reduce(function (total, next) { return total.concat(next.value.types); }, []),
                     allFunctions = dataProperties.reduce(function (total, next) { return total.concat(next.value.functions); }, []);
-
-                // Add all types.
-                allTypes.forEach(function (typeDef) {
-                    var dataNode = dataGraphState.addNode(typeDef.id, { name: typeDef.names[0], data: typeDef, group: 0, visualNode: undefined }),
-                        visualNode = visualGraphState.addNode(typeDef.id, { name: typeDef.names[0], data: typeDef, group: 0, dataNodes: [] });
-
-                    dataNode.data.visualNode = visualNode;
-                    visualNode.data.dataNodes.push(dataNode);
-                });
-
-                // Add all base types and links from type to base type.
-                allTypes.forEach(function (typeDef) {
-                    if (typeDef.isa) {
-                        // Gets nodes for isa if they exist or creates if they don't.
-                        var dataNode = dataGraphState.addNode(typeDef.isa, { name: typeDef.isa, data: typeDef, group: 0, visualNode: undefined }),
-                            visualNode = visualGraphState.addNode(typeDef.isa, { name: typeDef.isa, data: typeDef, group: 0, dataNodes: [] });
-
-                        dataNode.data.visualNode = visualNode;
-                        visualNode.data.dataNodes.push(dataNode);
-
-                        dataNode = dataGraphState.addLinkByNodeId(typeDef.isa, typeDef.id, 1, { data: typeDef, group: 0, visualNode: undefined });
-                        visualNode = visualGraphState.addLinkByNodeId(typeDef.isa, typeDef.id, 1, { data: typeDef, group: 0, dataNodes: [] });
-
-                        dataNode.data.visualNode = visualNode;
-                        visualNode.data.dataNodes.push(dataNode);
-                    }
-                });
 
                 // Add all functions and input and output types.
                 allFunctions.forEach(function (fnDef) {
                     var visualFunctionNode = visualGraphState.addNode(fnDef.id, { name: fnDef.names[0], data: fnDef, group: 1, dataNodes: [] });
 
-                    fnDef.templates.forEach(function (template, idx) {
-                        var dataFunctionNode = dataGraphState.addNode(fnDef.id + "-" + idx, { name: fnDef.names[0] + " (" + idx + ")", data: template, group: 1, visualNode: undefined });
+                    fnDef.templates.forEach(function (template, idx, templates) {
+                        var nodeId = fnDef.id,
+                            nodeName = fnDef.names[0];
+
+                        if (templates.length > 1) {
+                            nodeId += "-" + idx;
+                            nodeName += " (" + idx + ")";
+                        }
+
+                        var dataFunctionNode = dataGraphState.addNode(nodeId, { name: nodeName, data: template, group: 1, visualNode: undefined });
 
                         dataFunctionNode.data.visualNode = visualFunctionNode;
                         visualFunctionNode.data.dataNodes.push(dataFunctionNode);
@@ -86,8 +66,8 @@
                 allFunctions.forEach(function (fnDef) {
                     var visualFnId = fnDef.id;
 
-                    fnDef.templates.forEach(function (template, idx) {
-                        var dataFnId = fnDef.id + "-" + idx;
+                    fnDef.templates.forEach(function (template, idx, templates) {
+                        var dataFnId = fnDef.id + (templates.length > 1 ? + "-" + idx : "");
 
                         template.in.forEach(function (inType) {
                             var dataNode = dataGraphState.addLinkByNodeId(dataFnId, inType.type, 1, { data: undefined, group: 0, visualNode: undefined }),
